584a8a3307c8f1320b499874f059299a2dde6186
24swetaa <sweta99.ar@gmail.com>
2022-12-17 13:56:56 +0530

    edited project section

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './projects.css'
+import Pic from '../../assests/pics.png'
+import Movie from '../../assests/movie.png'
+import Shop from '../../assests/shop.png'
+import Monster from '../../assests/monster.png'
 const Projects = () => {
   return (
     <section id='projects'>
@@ -9,6 +13,7 @@ const Projects = () => {
           <h3>Jagriti Shop</h3>
           <h3>A cloth store management system</h3>
           <h4>SpringBoot, JDBC, MySQL, Thymeleaf</h4>
+          <img src={Shop} alt="" />
           <h5>Developed a full stack online shopping website for clothes. It has the register, login, and logout feature for both user and the admin. Users can view the products
 category and brand-wise and can place orders by adding them to their cart. Admin can manage employees, brands, categories and products and can update them.</h5>
           <a href='https://github.com/24swetaa/Clothes_Management_System' className='btn' target='_blank'>Github</a>
@@ -17,6 +22,7 @@ category and brand-wise and can place orders by adding them to their cart. Admin
           <h3>Movie Matrix</h3>
           <h3>A movie recommendation system</h3>
           <h4>Python, Streamlit, Machine learning</h4>
+          <img src={Movie} alt="" />
           <h5>Designed a web application which recommends 8 similar movies on the basis of a selected movie. Used content-based machine learning filtering algorithms. Fetched movie details and posters through TMDB website APIs. Implemented a chatbot which can answer queries of a user related to movie recommendations.</h5>
           <a href='https://github.com/24swetaa/Movie.Matrix' className='btn' target='_blank'>Github</a>
         </article>
@@ -24,6 +30,7 @@ category and brand-wise and can place orders by adding them to their cart. Admin
           <h3>Monster Shooting</h3>
           <h3>A game using genetic algorithm</h3>
           <h4>JavaScript, Genetic Algorithm</h4>
+          <img src={Monster} alt="" />
           <h5>This game mutates the monster (difficulty level of the game) by using a genetic algorithm. Main functions are encoding the population, population generation, fitness evaluation, selection of parents,
 crossover, mutation, elitism and evolution.</h5>
           <a href='https://github.com/24swetaa/monster_shooting_game' className='btn' target='_blank'>Github</a>
@@ -32,6 +39,7 @@ crossover, mutation, elitism and evolution.</h5>
           <h3>Pics Era</h3>
           <h3>Picture sharing web application</h3>
           <h4>Django, MySQL, HTML, CSS</h4>
+          <img src={Pic} alt="" />
           <h5>Built a picture-sharing web app using Django framework, where users can upload pictures category-wise. User can also create a new category.
 Used MySQL for the backend.</h5>
           <a href='https://github.com/24swetaa/picera' className='btn' target='_blank'>Github</a>
