70d23a72e4de78694ecaf326da434fb5884bbfa3
24swetaa <sweta99.ar@gmail.com>
2022-12-19 21:44:56 +0530

    edited about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,14 +11,14 @@ const About = () => {
         
         </div>
         <div className="about__content">
-          <h3><div className='quotes'>
-          Hi! I'm Sweta Singh.
-          </div>
-          </h3>
           <p>
-            I am a third year undergraduate student of the department of Computer Science and Engineering, Indian Institute of Technology (BHU) Varanasi, India. I was born in Ranchi, Jharkhand.
-            I like watching series and documentaries (especially of the thriller genre) and listening to stories. My usual way of going about everything in life is: <div className='quotes'><i>"Sing like no one’s listening, love like you’ve never been hurt, dance like nobody’s watching, and live like it’s heaven on earth."</i></div>
+        <span className='quotes'>Hi, I'm Sweta,</span> pursuing B.Tech in Computer Science and Engineering from IIT (BHU) Varanasi.
+An enthusiastic student with a strong affinity for the field of technology, data structures and algorithms, full stack development and machine learning. I have recently done projects in ReactJs, Django, Streamlit, SpringBoot, and Android Studio.
+I am passionate about building some interesting applications that will influence the real world for a good cause. I enjoy the time when I am creating any application. This helps me get comfortable with new technologies, databases, and how to handle bugs.
+I am interested in working in this field because it is full of challenges, and I will get to learn many skills in this ever-growing tech industry. In today's world, the demand for technology is high and I will be excited to contribute to this field. 
+
           </p>
+          <p><span className='quotes'><i>Would love to interact further!</i></span></p>
           {/* <a href='#contact' className='btn btn-primary' >Let's Talk</a> */}
         </div>
       </div>
